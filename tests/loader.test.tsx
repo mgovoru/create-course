ffe18e8ef6b556004ba892e86409b00c03c9416d
Maria Govorukhina <m.govoru@yandex.ru>
2024-07-24 17:47:51 +0500

    feat: add tests for error component

--- tests/loader.test.tsx
+++ tests/loader.test.tsx
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux'
 import store from '../src/components/Store/store'
 import { DetailsView } from '../src/components/DetailsView/DetailsView'
 import { Flyelement } from '../src/components/FlyElement/Flyelement'
+import { UnknownPage } from '../src/components/404/404'
+import { Pagination } from '../src/components/ListView/Pagination'
+import { ErrorBoundary } from '../src/components/ErrorBoundary/ErrorBoundary'
 
 describe('ListView', () => {
   it('renders the component', () => {
@@ -47,6 +50,34 @@ describe('ListView', () => {
         </Provider>
       )
       const block = screen.findByTestId('details-hero')
+      expect(block).not.toBeNull()
+    })
+     it('renders the component', () => {
+       render(
+         <Provider store={store}>
+           <UnknownPage />
+         </Provider>
+       )
+       const block = screen.findByTestId('unknown-title')
+       expect(block).not.toBeNull()
+     })
+      it('renders the component', () => {
+        render(
+          <Provider store={store}>
+            <Pagination totalPages={10} />
+          </Provider>
+        )
+        const block = screen.findByTestId('pagination')
+        expect(block).not.toBeNull()
+      })
+    it('renders the component', () => {
+      render(
+        <Provider store={store}>
+          <ErrorBoundary children={undefined}>
+          </ErrorBoundary>
+        </Provider>
+      )
+      const block = screen.findByTestId('error-title')
       expect(block).not.toBeNull()
     })
 })
