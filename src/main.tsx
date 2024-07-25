adf5b8ab567b8589b624ea789178eab43e3bdcb6
Maria Govorukhina <m.govoru@yandex.ru>
2024-07-25 16:36:57 +0500

    feat: test for slice add

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './null.scss'
-import './index.scss'
-import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx'
-import store from './components/Store/store.tsx'
-import { Provider } from 'react-redux'
+import { RenderApp } from './index.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </Provider>
-  </React.StrictMode>
-)
+RenderApp('root')
--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import App from '../src/App'
 import { Provider } from 'react-redux'
 import store from '../src/components/Store/store'
+import { RenderApp } from '../src'
 
 describe('App', () => {
   it('renders the component', () => {
@@ -12,5 +13,10 @@ describe('App', () => {
     )
     const element = screen.getByText('Characters')
     expect(element).not.toBeNull()
+  })
+  it('renders the component', () => {
+      const id = 'root'
+    document.body.innerHTML = `<div id="${id}"></div>`
+    expect(() => RenderApp(id)).not.toThrow()
   })
 })
